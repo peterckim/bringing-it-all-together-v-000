cb9ae5532a9a99ae909c277ae4365fd11c7d172f
Peter Kim <peterckim96@gmail.com>
2018-07-07 01:30:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,7 +67,7 @@ class Dog
     
     if !dog.empty?
       dog_data = dog[0]
-      dog = Dog.new(name: row[1], breed: row[2], id: row[0])
+      dog = Dog.new(name: dog_data[1], breed: dog_data[2], id: dog_data[0])
     else
       dog = self.create(name: name, breed: breed)
     end
